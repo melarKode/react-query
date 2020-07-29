63cf8bac48a8fd400df567b8b95a25dae477c308
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-07-29 10:04:48 +0530

    React-query

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {useQuery} from 'react-query';
+import Person from './Person';
+
+const fetchPeople = async()=>{
+    const res = await fetch('https://swapi.dev/api/people/');
+    return res.json();
+}
 
 export default function People() {
+    const {data, status} = useQuery('people', fetchPeople);
+    console.log(data)
     return (
         <div>
             <h2>People</h2>
+            {/* <p>{ status }</p> */}
+            {status==="loading" && (
+                <div>
+                    Loading Data
+                </div>
+            )}
+            {status==="error" && (
+                <div>
+                    Error Fetching Data
+                </div>
+            )}
+            {status==="success" && (
+                <div>
+                    {
+                        data.results.map(person=><Person key={person.name} person={person}/>)
+                    }
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {useQuery} from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async()=>{
+    const res = await fetch('http://swapi.dev/api/planets/');
+    return res.json();
+}
 
 export default function Planets() {
+    const {data, status} = useQuery('planets', fetchPlanets);
+    console.log(data)
     return (
         <div>
             <h2>Planets</h2>
+            {/* <p>{ status }</p> */}
+            {status==="loading" && (
+                <div>
+                    Loading Data
+                </div>
+            )}
+            {status==="error" && (
+                <div>
+                    Error Fetching Data
+                </div>
+            )}
+            {status==="success" && (
+                <div>
+                    {
+                        data.results.map(planet=><Planet key={planet.name} planet={planet}/>)
+                    }
+                </div>
+            )}
         </div>
     )
 }
